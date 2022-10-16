d98b0f9b05d13aa587019ee6fcdabbc4bf553b3b
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-16 13:03:24 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -10,8 +10,8 @@ from FallenRobot import telethn as tbot
 from FallenRobot.events import register
 
 PHOTO = [
-    "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+    "https://te.legra.ph/file/346f237b352ca3fb988cb.jpg",
+    "https://te.legra.ph/file/346f237b352ca3fb988cb.jpg",
 ]
 
 
